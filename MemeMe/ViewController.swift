356a18cae09ad727b170adca75b3d3368ccc0b89
Matt Henry <matt@henrymatt.com>
2019-12-02 14:08:37 -0700

    Add buttons to take or choose picture (not implemented)

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func choosePicture(_ sender: Any) {
+        print("Choosing a picture")
+    }
+    
+    @IBAction func selectPicture(_ sender: Any) {
+        print("Select a picture")
+    }
 
 
 }
