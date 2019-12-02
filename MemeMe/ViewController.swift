3a66b44633ed2cf56bcde0ad8b09f39afea7ceb7
Matt Henry <matt@henrymatt.com>
2019-12-02 13:25:48 -0700

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Matt Henry on 12/2/19.
+//  Copyright © 2019 Beardbird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
