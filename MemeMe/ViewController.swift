ee753ea5b127a6a11eeda1ed6f2faec5c1ee917a
Matt Henry <matt@henrymatt.com>
2019-12-03 14:21:10 -0700

    Change buttons to BarButtonItems, fix keyboard position

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var takePictureButton: UIButton!
     @IBOutlet weak var displayImageView: UIView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var deleteImageButton: UIButton!
+    @IBOutlet weak var deleteImageButton: UIBarButtonItem!
+    @IBOutlet weak var shareMemeButton: UIBarButtonItem!
     
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
+    
+    var currentlyEditingTextField: UITextField?
     
     let defaultText = "ENTER TEXT"
     
@@ -28,14 +31,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         setUpTextFields()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
     func setUIForImageEditingState() {
         displayImageView.isHidden = false
-        deleteImageButton.isHidden = false
+        shareMemeButton.isEnabled = true
+        deleteImageButton.isEnabled = true
     }
     
     func setUIForNoImageState() {
         displayImageView.isHidden = true
-        deleteImageButton.isHidden = true
+        shareMemeButton.isEnabled = false
+        deleteImageButton.isEnabled = false
         imageView.image = nil
     }
     
@@ -78,12 +93,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentlyEditingTextField = textField
         if textField.text == defaultText {
             textField.text = ""
         }
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        currentlyEditingTextField = nil
         if (textField.text == "") {
             textField.text = defaultText
         }
@@ -111,7 +128,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func deleteImage() {
         setUIForNoImageState()
     }
+    
+    @IBAction func shareMeme() {
+        
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if (currentlyEditingTextField == bottomText) {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
 
-
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
 }
 
