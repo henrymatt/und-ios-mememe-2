8317fb92cb026e80b2c004689e1ba6d660382360
Matt Henry <matt@henrymatt.com>
2019-12-04 08:27:14 -0700

    Shrink to fit text

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -97,7 +97,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 30)!,
-            NSAttributedString.Key.strokeWidth: -1.0,
+            NSAttributedString.Key.strokeWidth: -2.0,
             NSAttributedString.Key.paragraphStyle: memeParagaphAttributes
         ]
         
